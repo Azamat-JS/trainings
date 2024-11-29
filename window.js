62dda728bb06b4078d44afe2e912fcf63ab143fa
Hi <azamatielts987@gmail.com>
2024-11-29 06:08:25 +0500

    window functions

---
+++ window.js
@@ -0,0 +1,48 @@
+//------------------ opening another web --------------
+
+// const button = document.getElementById("button")
+// button.style.fontSize = "5rem"
+
+// button.addEventListener("click", function(evt) {
+//     evt.preventDefault()
+//     location.replace("https://www.google.com/maps/@41.2206582,64.7164746,10z?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D")
+// })
+
+//---------- shifrlash ----------------
+
+// console.log(atob("You are from India"));
+// console.log(btoa("b­çë¢b'v&"));
+
+//--------------- info on screen -------------
+
+// console.log(window.screen);
+// console.log(window.screen.height);
+
+//------------- info on battery ------------
+// button.addEventListener("click", function(evt) {
+//     evt.preventDefault()
+// window.navigator.getBattery().then((battery) => {
+// console.log(battery.level * 100);
+// }) 
+// })
+
+//----------------- geoposition ------------
+
+// window.navigator.geolocation.watchPosition((position) =>{
+//     console.log(position.coords);  
+// }, (error) => {
+//     console.log(error);
+    
+// })
+
+//------------ clipboard ------------------
+
+// window.navigator.clipboard.writeText("running").then(() =>{
+//     console.log("well done");    
+// }, function(err) {
+//     console.error("error");
+    
+// })
+
+
+
