1d9c79d660a63f77d425a2dd89ef83f57890e6b5
Hi <azamatielts987@gmail.com>
2024-11-30 20:27:42 +0500

    added prototypes

---
+++ oop.js
@@ -0,0 +1,98 @@
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age
+// }
+// Person.prototype.multiply = function(){
+//     console.log(`${this.name} will be ${this.age * 2} by 2048`);
+// }
+
+// const ageCount = new Person("Ali", 30)
+// ageCount.multiply()
+
+//-------------- string prototype -----------------
+
+// String.prototype.palindrom = function(){
+// console.log(this == this.split("").reverse().join(""))
+// }
+
+// let s = "Meva"
+// let a = "eve"
+
+// a.palindrom()
+//----------------- object prototype ------------------
+
+// Object.prototype.findNames = function(){
+//     for (const key in this) {
+//        if(key === "name"){
+//         return this[key]
+//        }
+            
+//         }
+//     }
+// let obj = {
+//     name: "Aziz",
+//     age: 26
+// }
+// console.log(obj.findNames());
+
+//-------------------- object prot ----
+
+// function MyObj (name){
+//     this.name = name
+// }
+
+// MyObj.prototype.verifying = function(){
+//     return `${this.name} you joined to this group`
+// }
+// const person1 = new MyObj("Ali")
+// const person2 = new MyObj("Aziz")
+// console.log(person1.verifying())
+// console.log(person2.verifying())
+//--------------------  upgrading objects ------------
+
+// function Laptop(brand, ram, program){
+//     this.brand = brand,
+//     this.ram = ram
+//     this.program = program
+// }
+
+// Laptop.prototype.upgrades = function(newRam, newProgram){
+// this.ram = newRam
+// this.program = newProgram
+// }
+
+// const myLaptop = new Laptop("Mac", "8GB", "Linux")
+// myLaptop.upgrades("16GB","IOS")
+// console.log(myLaptop);
+
+//----------------- constructo - --------------
+
+// function Student (name){
+//     this.name = name
+// }
+
+// Student.prototype.grades = [90, 80, 86]
+
+// const student1 = new Student("Ali")
+// const student2 = new Student("Anvar")
+
+// student1.grades.push(95)
+
+// console.log(student1.grades)
+
+//----------------- contstructor --------------
+
+// function Phone(model){
+//     this.model = model
+//     this.showModel = function(){
+//   console.log(`"Model is ${this.model}"`);
+  
+//     }
+// }
+// Phone.prototype.showBrand = function(brand){
+// console.log(`Brand is ${brand}`);
+
+// }
+// const phone = new Phone ("Iphone 16")
+// phone.showModel()
+// phone.showBrand("Apple")
