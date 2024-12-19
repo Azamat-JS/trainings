09750351526fd4093b6d3c978de17367d195ab3b
Azamat-JS <azamatielts987@gmail.com>
2024-12-19 21:25:15 +0500

    added code

--- code.js
+++ code.js
@@ -237,4 +237,17 @@
 // console.log(measureDepth([[[[[[[]]]]]]]));   
 // console.log(measureDepth([1, 2, 3]))
 
+//------------- currying
 
+// function curry(f){
+//   return function(a){
+//     return function(b){
+//       return f(a, b)
+//     }
+//   }
+// }
+// function sum(a, b){
+//   return a + b
+// }
+// let curriedSum = curry(sum)
+// console.log(curriedSum(1)(2)); // 3
