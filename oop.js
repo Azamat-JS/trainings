be2a71a5f83d2a467da91ad9d0a2870a9504a07b
Azamat-JS <azamatielts987@gmail.com>
2024-12-22 22:35:06 +0500

    class type

--- oop.js
+++ oop.js
@@ -109,5 +109,27 @@
 // myObj.money = 5000
 // myObj["address"] = "Ulug'bek"
 // console.log(myObj);
-//------------------------- 
+//------------------------- class expression -----
+
+class User {
+    constructor(name){
+    this.name = name
+}
+get name(){
+return this._name
+}
+set name(value){
+    if(value.length < 4){
+        console.log("name is too short");
+        return
+    }
+    this._name = value
+}
+}
+// let user = new User("John")
+// console.log(user); // John
+
+let user = new User("Ali")
+console.log(user);
+
 
