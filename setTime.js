4ad4e3d3e0a026992b3efe93aa60c22bab89acf1
Azamat-JS <azamatielts987@gmail.com>
2024-12-30 22:57:02 +0500

    added change

--- setTime.js
+++ setTime.js
@@ -207,11 +207,20 @@
 
 //----------------------
 
-function mul(a, b){
-    return a*b
-}
-let double = mul.bind(null, 2)
+// function mul(a, b){
+//     return a*b
+// }
+// let double = mul.bind(null, 2)
 
-console.log(double(3));
-console.log(double(4));
-console.log(double(13));
+// console.log(double(3));
+// console.log(double(4));
+// console.log(double(13));
+
+//---------------------
+
+function f(){
+    console.log(this.name);
+}
+
+f = f.bind({name: "john"}).bind({name: "Pete"})
+f() // john
