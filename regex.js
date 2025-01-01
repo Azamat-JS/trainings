7751995174e404c2915d5b7156822bf1e7ece47e
Azamat-JS <azamatielts987@gmail.com>
2025-01-01 05:37:32 +0500

    find dots

--- regex.js
+++ regex.js
@@ -4,7 +4,10 @@
 
 // ------- 2 find time 
 
-let regex = /\d\d[-:]\d\d/g
-console.log("Breakfast at 09:00. Dinner at 21-30".match(regex)); // ['09:00', '21-30']
+// let regex = /\d\d[-:]\d\d/g
+// console.log("Breakfast at 09:00. Dinner at 21-30".match(regex)); // ['09:00', '21-30']
 
+// ---------- 3 find dots
+let regexp = /\.{3,}/g
+console.log("Salom!... How goes?....".match(regexp));
 
