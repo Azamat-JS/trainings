cb5d8885efede3c3bd074f5529da912460c7481a
Azamat-JS <azamatielts987@gmail.com>
2025-01-01 05:51:13 +0500

    find digit

--- regex.js
+++ regex.js
@@ -8,6 +8,13 @@
 // console.log("Breakfast at 09:00. Dinner at 21-30".match(regex)); // ['09:00', '21-30']
 
 // ---------- 3 find dots
-let regexp = /\.{3,}/g
-console.log("Salom!... How goes?....".match(regexp));
+// let regexp = /\.{3,}/g
+// console.log("Salom!... How goes?....".match(regexp));
 
+// ----------- 4 find digit
+
+// let str = "+998(91)-092-83-53"
+// let regex = /\d/g
+// console.log(str.match(regex)); // [9,9,8,9,1,0,9,2,8,3,5,3]
+
+// console.log(str.match(regex).join(""));
