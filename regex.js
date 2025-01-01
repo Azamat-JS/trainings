dc72237bbbd38fd09171f8f244f65ee4a74815a1
Azamat-JS <azamatielts987@gmail.com>
2025-01-01 05:34:29 +0500

    find time with regex

--- regex.js
+++ regex.js
@@ -1,3 +1,10 @@
-let a = "JavaScript"
-console.log(a.match(/Java[^script]/)); // JavaS
+// -------------- 1
+// let a = "JavaScript"
+// console.log(a.match(/Java[^script]/)); // JavaS
+
+// ------- 2 find time 
+
+let regex = /\d\d[-:]\d\d/g
+console.log("Breakfast at 09:00. Dinner at 21-30".match(regex)); // ['09:00', '21-30']
+
 
