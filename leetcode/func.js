fde51fa08b99abd71f4a68903e86a85f649efa97
Azamat-JS <azamatielts987@gmail.com>
2025-02-13 19:31:31 +0500

    solved leet problems

--- leetcode/func.js
+++ leetcode/func.js
@@ -71,5 +71,49 @@
 // console.log(ex.tobe(10));
 // console.log(ex.notToBe(5));
 
+//--------------------------------
 
+// let createCounter = function(init){
+//     let presentCount = init
+//     return {
+//         increment:()=> ++presentCount,
+//         decrement:()=> --presentCount,
+//         reset:() => presentCount = init
+//     }
+// };
+// const c = createCounter(3)
+// console.log(c.decrement());
+// console.log(c.reset());
+// console.log(c.increment());
 
+// let map = function(arr, fn){
+//     const array = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         array[i] = fn(arr[i], i);  
+//     }
+//     return array;
+// }
+
+// //^^^^^^^^^^^^^^^^
+
+// const nums = [1, 2, 3, 4];
+// const double = function(n){
+//     return n * 2
+// };
+// console.log(map(nums, double));
+// console.log(map(nums, (n, i) => n + i));
+// console.log(map(nums, (n) => 42 ));
+
+//----------------
+
+// let filter = function(arr, fn){
+//   let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if(fn(arr[i], i)){
+//             result.push(arr[i])
+//         } 
+//     }
+//     return result
+// }
+// const t = [1,2, 3, 6]
+// console.log(filter(t, (v) => v % 2 === 0));
