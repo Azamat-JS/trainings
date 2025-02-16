be3089c05832157b37eb9426c9560f3c0f081047
Azamat-JS <azamatielts987@gmail.com>
2025-02-16 22:07:17 +0500

    checking obj and arr if is empty

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -1,8 +1,25 @@
-let compose = function(functions){
-    return function(x){
-        return functions.reduceRight((acc, fn) => fn(acc), x)
-    }
-}
-const result = compose([x => x + 1, x => x * x, x => 2 * x])
-console.log(result(4)); // 65
+// let compose = function(functions){
+//     return function(x){
+//         return functions.reduceRight((acc, fn) => fn(acc), x)
+//     }
+// }
+// const result = compose([x => x + 1, x => x * x, x => 2 * x])
+// console.log(result(4)); // 65
 
+//--------------- isEmpty
+
+// let isEmpty = function(obj){
+//     for(let key in obj)
+//         return false;
+//     return true;
+// }
+
+// let object = {
+//     name: 'ali'
+// }
+// let arr = [true]
+
+// let obje = {}
+// let array = []
+
+// console.log(isEmpty(array));
