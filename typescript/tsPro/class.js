6a1672c1f2f9e6f935c53881f314a16b48de5098
Azamat-JS <azamatielts987@gmail.com>
2025-02-22 21:33:32 +0500

    injection class

--- typescript/tsPro/class.js
+++ typescript/tsPro/class.js
@@ -1,24 +1,51 @@
 ///-------- getters and setters
-var Circle = /** @class */ (function () {
-    function Circle(radius) {
-        this._radius = radius;
+// class Circle {
+//     private _radius:number;
+//     constructor(radius:number){
+//         this._radius = radius;
+//     }
+//     get radius():number{
+//      return this._radius
+//     }
+//     set radius(value:number){
+//         if(value <= 0){
+//             throw new Error('Radius must be positive')
+//         }
+//         this._radius = value
+//     }
+// }
+// const circle = new Circle(11)
+// console.log(circle.radius);
+// circle.radius = 17
+// console.log(circle.radius);
+//----------- static 
+// class MathUtils{
+//     static PI = 3.14;
+//     static calculateArea(radius:number){
+//         return MathUtils.PI * radius * radius
+//     }
+// }
+// console.log(MathUtils.PI);
+// console.log(MathUtils.calculateArea(10));
+//  ---- injection
+var Engine = /** @class */ (function () {
+    function Engine() {
     }
-    Object.defineProperty(Circle.prototype, "radius", {
-        get: function () {
-            return this._radius;
-        },
-        set: function (value) {
-            if (value <= 0) {
-                throw new Error('Radius must be positive');
-            }
-            this._radius = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Circle;
+    Engine.prototype.start = function () {
+        console.log('Engine started');
+    };
+    return Engine;
 }());
-var circle = new Circle(11);
-console.log(circle.radius);
-circle.radius = 17;
-console.log(circle.radius);
+var Car = /** @class */ (function () {
+    function Car(engine) {
+        this.engine = engine;
+    }
+    Car.prototype.drive = function () {
+        this.engine.start();
+        console.log('Car is driving');
+    };
+    return Car;
+}());
+var engine = new Engine();
+var car = new Car(engine);
+car.drive();
--- typescript/tsPro/class.ts
+++ typescript/tsPro/class.ts
@@ -24,3 +24,36 @@
 // circle.radius = 17
 // console.log(circle.radius);
 
+//----------- static 
+
+// class MathUtils{
+//     static PI = 3.14;
+
+//     static calculateArea(radius:number){
+//         return MathUtils.PI * radius * radius
+//     }
+// }
+
+// console.log(MathUtils.PI);
+// console.log(MathUtils.calculateArea(10));
+
+//  ---- injection
+
+// class Engine {
+//     start():void{
+//         console.log('Engine started');
+//     }
+// }
+
+// class Car {
+//     constructor(private engine: Engine){}
+
+//     drive():void{
+//         this.engine.start()
+//         console.log('Car is driving');
+//     }
+// }
+
+// const engine = new Engine();
+// const car = new Car(engine)
+// car.drive()
