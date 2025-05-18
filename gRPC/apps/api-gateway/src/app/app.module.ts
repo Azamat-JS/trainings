c2c207aef921beee613472cf15ec8055fd1a82d8
Azamat-JS <azamatielts987@gmail.com>
2025-05-18 21:31:19 +0500

    gRPC connection implemented

--- gRPC/apps/api-gateway/src/app/app.module.ts
+++ gRPC/apps/api-gateway/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule } from '@nestjs/microservices';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PRODUCTS_PACKAGE_NAME } from '../constants';
+import { join } from 'path';
+import { ProductController } from './product/product.controller';
 
 @Module({
   imports: [ClientsModule.register([
     {
-      name: PRODUCTS_PACKAGE_NAME
+      name: PRODUCTS_PACKAGE_NAME,
+      transport: Transport.GRPC,
+      options: {
+        package: PRODUCTS_PACKAGE_NAME,
+        protoPath: join(__dirname, 'proto/products.proto')
+      }
     }
   ])],
-  controllers: [AppController],
+  controllers: [AppController, ProductController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ gRPC/apps/api-gateway/src/app/product/product.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Inject, OnModuleInit, Param } from '@nestjs/common';
+import { PRODUCTS_PACKAGE_NAME } from '../../constants';
+import { ClientGrpc } from '@nestjs/microservices';
+import {PRODUCT_SERVICE_NAME, ProductServiceClient} from 'types/proto/products'
+
+@Controller('product')
+export class ProductController implements OnModuleInit{
+    private productService: ProductServiceClient;
+    constructor(@Inject(PRODUCTS_PACKAGE_NAME) private client: ClientGrpc){}
+
+    onModuleInit() {
+        this.productService = this.client.getService<ProductServiceClient>(PRODUCT_SERVICE_NAME)
+    }
+
+    @Get()
+    findOne(){
+        return this.productService.getProduct({productId: 12})
+    }
+}
