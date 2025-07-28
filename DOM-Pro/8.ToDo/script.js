ae034df1bd3fccd4292f2ab2f16481ed9f502f55
Azamat-JS <azamatielts987@gmail.com>
2025-07-28 10:58:24 +0500

    COmpleted ToDo app

--- DOM-Pro/8.ToDo/script.js
+++ DOM-Pro/8.ToDo/script.js
@@ -45,9 +45,8 @@ function saveTodos() {
 
 function updateItemsCount() {
   const uncompletedTodos = todos.filter((todo) => !todo.completed);
-  itemsLeft.textContent = `${uncompletedTodos?.length} item${
-    uncompletedTodos?.length !== 1 ? "s" : ""
-  } left`;
+  itemsLeft.textContent = `${uncompletedTodos?.length} item${uncompletedTodos?.length !== 1 ? "s" : ""
+    } left`;
 }
 
 function checkEmptyState() {
@@ -66,3 +65,107 @@ function filterTodos(filter) {
       return todos;
   }
 }
+
+function renderTodos() {
+  todosList.innerHTML = "";
+
+  const filteredTodos = filterTodos(currentFilter);
+  filteredTodos.forEach(todo => {
+    const todoItem = document.createElement('li');
+    todoItem.classList.add('todo-item');
+    if (todo.completed) todoItem.classList.add('completed');
+
+    const checkboxContainer = document.createElement('label');
+    checkboxContainer.classList.add('checkbox-container');
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('todo-checkbox');
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', () => toggleTodo(todo.id));
+
+    const checkmark = document.createElement('span');
+    checkmark.classList.add('checkmark');
+
+    checkboxContainer.appendChild(checkbox)
+    checkboxContainer.appendChild(checkmark)
+
+    const todoText = document.createElement('span');
+    todoText.classList.add('todo-item-text');
+    todoText.textContent = todo.text;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+    deleteBtn.addEventListener("click", () => deleteTodo(todo.id));
+
+    todoItem.appendChild(checkboxContainer);
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(deleteBtn);
+
+    todosList.appendChild(todoItem);
+  })
+}
+
+function clearCompleted() {
+  todos = todos.filter(todo => !todo.completed);
+  saveTodos()
+  renderTodos()
+ }
+
+
+function deleteTodo(id) {
+  todos = todos.filter(todo => todo.id !== id)
+  saveTodos();
+  renderTodos();
+}
+
+
+function toggleTodo(id) {
+  todos = todos.map((todo) => {
+    if (todo.id === id) {
+      return { ...todo, completed: !todo.completed }
+    }
+    return todo;
+  });
+  saveTodos();
+  renderTodos();
+}
+
+function loadTodos() {
+  const storedTodos = localStorage.getItem('todos')
+  if (storedTodos) todos = JSON.parse(storedTodos)
+  renderTodos();
+}
+
+filters.forEach(filter => {
+  filter.addEventListener('click',  () => {
+    setActiveFilter(filter.getAttribute('data-filter'))
+  })
+});
+
+function setActiveFilter(filter){
+  currentFilter = filter;
+
+  filters.forEach((item) => {
+    if(item.getAttribute('data-filter') === filter){
+      item.classList.add('active');
+    }else{
+      item.classList.remove('active')
+    }
+  });
+  renderTodos();
+}
+
+function setDate(){
+  const options = {weekday: "long", month:'short', day: 'numeric'};
+  const today = new Date();
+
+  dateElement.textContent = today.toLocaleDateString("en-US", options)
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadTodos();
+  updateItemsCount();
+  setDate()
+})
