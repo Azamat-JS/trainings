028cbedeb99b3cd01c5b6930856be07f9ee6acab
Azamat-JS <azamatielts987@gmail.com>
2025-08-02 23:05:51 +0500

    Implemented toggle complete button with Redux

--- habit-tracking/src/components/HabitList.tsx
+++ habit-tracking/src/components/HabitList.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import type { RootState } from '../app/store'
-import { Box, Grid, Paper, Typography } from '@mui/material'
+import { Box, Button, Grid, Paper, Typography } from '@mui/material'
+import CheckCircleIcon  from '@mui/icons-material/CheckCircle'
+import DeleteIcon  from '@mui/icons-material/Delete'
+import { toggleHabit } from '../app/habitSlice'
 
 const HabitList:React.FC = () => {
-  const {habits} =  useSelector((state:RootState) => state.habits)
+  const {habits} =  useSelector((state:RootState) => state.habits);
+  const dispatch = useDispatch();
+
+  const today = new Date().toISOString().split('T')[0];
+
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', gap: 2, mt: 4}}>
         {habits.map((habit) => {
             return <Paper key={habit.id} elevation={2} sx={{p: 2}}>
-                <Grid container alignItems="center">
+                <Grid container alignItems="center" justifyContent="space-between" spacing={2}>
                     <Grid>
                         <Typography variant='h6'>{habit.name}</Typography>
                         <Typography variant='body2' color='text.secondary'>
@@ -17,7 +24,26 @@ const HabitList:React.FC = () => {
                         </Typography>
                     </Grid>
                     <Grid>
-                        <Typography></Typography>
+                        <Box sx={{display: "flex", justifyContent: 'flex-end', gap: 1}}>
+                            <Button variant='outlined'
+                                color={
+                                    habit.completedDates.includes(today) ? 'success' : 'primary'
+                                }
+                                startIcon={<CheckCircleIcon/>}
+                                onClick={() => dispatch(toggleHabit({id: habit.id, date:today}))}
+                            >
+                                {habit.completedDates.includes(today) 
+                                ? "Completed"
+                                : "Mark Complete"}
+                            </Button>
+                            <Button 
+                                variant='outlined'
+                                color='error'
+                                startIcon={<DeleteIcon/>}
+                                >
+                                Delete
+                            </Button>
+                        </Box>
                     </Grid>
                 </Grid>
             </Paper>
