8246f4e2e7ba7c9fe301eee3229e89951ead67ce
Azamat-JS <azamatielts987@gmail.com>
2025-08-02 22:39:26 +0500

    Added Grid MUI in Habit tracking app

--- habit-tracking/src/App.tsx
+++ habit-tracking/src/App.tsx
@@ -1,11 +1,18 @@
 import { Provider } from "react-redux"
 import store from './app/store.ts'
+import { Container, Typography } from "@mui/material"
+import AddHabitForm from "./components/AddHabitForm.tsx"
+import HabitList from "./components/HabitList.tsx"
 
 
 const App = () => {
     return (
         <Provider store={store}>
-            <div>App</div>
+            <Container maxWidth="md">
+                <Typography component="h1" variant="h2" align="center">Habit Tracker</Typography>
+                <AddHabitForm />
+                <HabitList />
+            </Container>
         </Provider>
     )
 }
